921c9f6a744fcca4edf19715f6e7e06b803ab54b
rakibul islam <rakibulislam325558@gmail.com>
2025-01-22 00:32:16 +0600

    method

--- javascprit string/Myscprit.js
+++ javascprit string/Myscprit.js
@@ -1,4 +1,4 @@
-let x ='Rakibul';
+let x ='Rakibul islam ';
 let y = 'Rahim';
 
 console.log(x === y);
@@ -6,3 +6,4 @@ console.log(x === y);
 const Care ={Name:"BME", Color:"Red",Country:"German", };
 
 console.log(Care.Country+Date())
+console.log(x.slice(8,15))
