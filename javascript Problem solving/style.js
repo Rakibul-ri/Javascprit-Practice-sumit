2f45d61f76d7307c92baadd318ba349ba7e15a7b
rakibul islam <rakibulislam325558@gmail.com>
2025-09-04 23:31:43 +0600

    new Problem solved

--- javascript Problem solving/style.js
+++ javascript Problem solving/style.js
@@ -4,4 +4,17 @@
 console.log(getRndNumber(1,6)); 
 
 const Students = ["Rakibul", "Sakibul", "Akash", "Nahid", "Rubel"];
-console.log(Students.sort());
+console.log(Students.sort()); 
+
+function isleapyear(year){
+  if ((year % 400 ===0) || ((year% 4 ===0) && (year % 100 !==0))){
+    console.log (`${year} is Leapyear `);
+
+  }
+  else{
+    console.log (`${year} is not Leapyear `);
+  }
+
+}
+
+isleapyear(2032);
